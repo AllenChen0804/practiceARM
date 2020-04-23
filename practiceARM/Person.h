2f8bd6ec6e78dc84c79d47efaf290e53ec1690ba
Allen <allenchen08040614@gmail.com>
2020-04-23 18:07:55 +0800

    pointer

---
+++ practiceARM/Person.h
@@ -0,0 +1,20 @@
+//
+//  Person.h
+//  practiceARM
+//
+//  Created by 陳永展 on 2020/4/23.
+//  Copyright © 2020 allen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+/**  */
+@property(nonatomic, assign)int age;
+/**  */
+@property (nonatomic, copy) NSString *name;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ practiceARM/Person.m
@@ -0,0 +1,15 @@
+//
+//  Person.m
+//  practiceARM
+//
+//  Created by 陳永展 on 2020/4/23.
+//  Copyright © 2020 allen. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+
+
+@end
